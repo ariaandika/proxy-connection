7dd3830d0560259a2d11b54248107e3cedeea536
ariaandika <andikaakun17@gmail.com>
2023-12-21 21:01:19 +0700

    go proxy work with bun

--- go/main.go
+++ go/main.go
@@ -1,29 +1,58 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"strings"
 )
 
 func main() {
-  signleHostProxy()
+
+  port := getPort()
+
+  signleHostProxy(port)
 }
 
-func signleHostProxy() {
+func signleHostProxy(port string) {
   url, urlErr := url.Parse("http://localhost:8000")
-  httputil.NewSingleHostReverseProxy(url)
 
   if urlErr != nil {
     log.Fatal("Invalid url", url)
   }
 
-  httpErr := http.ListenAndServe(":3000", nil)
+  url.Host = url.Hostname() + ":" + port
+
+  proxy := httputil.NewSingleHostReverseProxy(url)
+
+  httpErr := http.ListenAndServe(":3000", proxy)
   
   if httpErr != nil {
     log.Fatal("Failed to listen and serve ", httpErr)
   }
 }
 
+func getPort() string {
+  envs := os.Environ()
+
+  for _, elem := range envs {
+    s := strings.Split(elem, "=")
+    if s[0] == "TARGET" {
+      return s[1]
+    }
+  }
+
+  log.Fatal("must provide TARGET env vars")
+  return ""
+
+  // file, err := os.ReadFile("../config")
+  // if err != nil { log.Fatal(err) }
+  //
+  // content := string(file)
+  //
+  // port := strings.Split(content, "\n")[0]
+  //
+  // return port
+}
